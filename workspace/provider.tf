d6524950319606c0b4ded6c0400bd8ad26dd4a4e
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-08-25 19:48:20 +0530

    workspace

--- workspace/provider.tf
+++ workspace/provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    # bucket = "daws-76-bhargav-tfstate"
-    # key    = "foreach"
-    # region = "us-east-1"
-    # dynamodb_table = "daws-tfstate-table"
+    bucket = "daws-76-bhargav-tfstate"
+    key    = "workspace"
+    region = "us-east-1"
+    dynamodb_table = "daws-tfstate-table"
   }
 
 }
